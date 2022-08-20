106479f6f82858359ed035d410cdf64488187b2a
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-20 13:20:07 +0200

    GET- request for readMore (single object) with credits.

--- src/services/TheMovieAPI.js
+++ src/services/TheMovieAPI.js
@@ -10,16 +10,53 @@ const API_KEY = import.meta.env.VITE_THE_MOVIE_API_KEY
 
 
 /**
+ * Get 
+ */
+
+const get = async (endpoint) => {
+    const res = await axios.get(BASE_URL + endpoint)
+
+    return res.data
+}
+
+/**
  * Get Popular Movies
  */
 
 const getPopularMovies = async () => {
-    const res = await axios.get(`${BASE_URL}/3/movie/popular?api_key=${API_KEY}`)
 
-    return res.data.results
+    return get(`/3/movie/popular?api_key=${API_KEY}&page=1`)
+
+}
+
+/**
+ * Get Top Rated Movies
+ */
+
+ const getTopRatedMovies = async () => {
+
+    return get(`/3/movie/top_rated?api_key=${API_KEY}&page=1`)
+
+}
+
+/**
+ * Get More Info
+ */
+
+ const getReadMore = async (movieId) => {
+
+    const info = get(`/3/movie/${movieId}?api_key=${API_KEY}&append_to_response=credits`)
+    console.log('MORE INFO', info   )
+
+
+    return info
+
+
 }
 
 // eslint-disable-next-line 
 export default {
     getPopularMovies,
+    getTopRatedMovies,
+    getReadMore,
 }
