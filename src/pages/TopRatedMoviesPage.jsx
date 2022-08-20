083c6fcdcf2c438b3206f111357bfd6d2fee613f
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-20 13:29:33 +0200

    Imported Movie component to TopRatedMovisPage + added movie.id to link path

---
+++ src/pages/TopRatedMoviesPage.jsx
@@ -0,0 +1,40 @@
+import Movies from '../components/Movies'
+import useTopRatedMovies from '../hooks/useTopRatedMovies'
+
+
+// styles
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+
+
+
+const TopRatedMoviesPage = () => {
+    const { data: movies, error, isError, isLoading, isSuccess } = useTopRatedMovies()
+
+	// console.log('TOP RATED MOVIES: ', movies)
+
+  	return (
+		<Container className="py-3">
+			<h1>Top Rates Movies</h1>
+
+			{isLoading && (<p className="my-3">Loading Movies...</p>)}
+
+			{isError && (
+				<Alert variant="danger">
+					<h3>Error!</h3>
+					<p>{error.message}</p>
+				</Alert>)
+			}
+				{movies &&  ( 
+					<Movies movies={movies} />	
+				)}
+
+
+		
+
+		
+		</Container> 
+	)
+}
+
+export default TopRatedMoviesPage
