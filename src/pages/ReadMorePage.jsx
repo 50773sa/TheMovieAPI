0969a945cfbf06b20a6a1f193553e69754d625b5
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-20 13:16:00 +0200

    Card with more details about a movie

---
+++ src/pages/ReadMorePage.jsx
@@ -0,0 +1,41 @@
+import ReadMore from '../components/ReadMore'
+import useReadMore from '../hooks/useReadMore'
+import { useParams } from 'react-router-dom'
+
+// styles
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+
+
+
+const ReadMorePage = () => {
+    const { id } = useParams() // för att den ska veta vilket id den ska hämta
+    const { data: moreInfo, error, isError, isLoading, isSuccess } = useReadMore(id)
+
+	console.log('More INFO: ', moreInfo)
+
+  	return (
+		<Container className="py-3">
+
+			{isLoading && (<p className="my-3">Loading...</p>)}
+
+			{isError && (
+				<Alert variant="danger">
+					<h3>Error!</h3>
+					<p>{error.message}</p>
+				</Alert>)
+			}
+
+			{moreInfo &&  ( 
+				<ReadMore moreInfo={moreInfo} />	
+			)}
+
+
+		
+
+		
+		</Container> 
+	)
+}
+
+export default ReadMorePage
