27905e4e558d4e256ee04ec67adce31bfe2948b5
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-21 14:17:23 +0200

    Page for latest movies.

---
+++ src/pages/LatestMoviesPage.jsx
@@ -0,0 +1,41 @@
+import Movies from '../components/Movies'
+import useLatestMovies from '../hooks/useLatestMovies'
+
+
+// styles
+import Container from 'react-bootstrap/Container'
+import Alert from 'react-bootstrap/Alert'
+
+
+
+const PopularMoviesPage = () => {
+    const { data: movies, error, isError, isLoading, isSuccess } = useLatestMovies()
+
+	console.log('POPULAR MOVIES: ', movies)
+
+  	return (
+		<Container className="py-3">
+			<h1>Latest Movies</h1>
+
+			{isLoading && (<p className="my-3">Loading Movies...</p>)}
+
+			{isError && (
+				<Alert variant="danger">
+					<h3>Error!</h3>
+					<p>{error.message}</p>
+				</Alert>)
+			}
+
+			{movies &&  ( 
+				<Movies movies={movies} />	
+			)}
+
+
+		
+
+		
+		</Container> 
+	)
+}
+
+export default PopularMoviesPage
