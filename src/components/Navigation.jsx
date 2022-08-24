04f4f100fb79b714ff79c7892b79545b28c26108
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-24 12:34:04 +0200

    Removed dropNav.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navigation = () => {
 	return (
@@ -18,28 +17,6 @@ const Navigation = () => {
 						<Nav.Link as={NavLink} end to="/popular-movies">Popular Movies</Nav.Link>
 						<Nav.Link as={NavLink} end to="/top-rated-movies">Top Rated</Nav.Link>
 						<Nav.Link as={NavLink} end to="/genres">Genres</Nav.Link>
-						
-						{/* <NavDropdown title="Genre" id="navbarScrollingDropdown">
-							<NavDropdown.Item as={NavLink} end to="/genre/action" >Action</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/adventure" >Adventure</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/animation" >Animation</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/comedy" >Comedy</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/crime" >Crime</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/documentary" >Documentary</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/drama" >Drama</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/family" >Family</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/fantasy" >Fantasy</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/history" >History</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/horror" >Horror</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/music" >Music</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/mystery" >Mystery</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/romance" >Romance</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/science-fiction" >Science Fiction</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/tv-movie" >TV Movie</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/thriller" >Thriller</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/war" >War</NavDropdown.Item>
-							<NavDropdown.Item as={NavLink} end to="/genre/western" >Western</NavDropdown.Item>
-						</NavDropdown> */}
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
