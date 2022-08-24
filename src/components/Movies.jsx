d54148af894f5bfa39824949d69187f511c70ed1
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-24 12:36:07 +0200

    Only some cleaning 🧹

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,37 +7,34 @@ import Row from 'react-bootstrap/Row'
 import { Link } from 'react-router-dom'
 
 
-
 const Movies = ({ movies }) => {
 
 	const imageUrl = 'https://image.tmdb.org/t/p/w500'
-	console.log('MOVIES', movies)
 	
   	return (
 
 		<Row>
-		{movies.results.map(movie => (
-			<Col lg={3} md={4} sm={6} key={movie.id}>
-					
-				<Card className="mb-4">
-					<Card.Img variant="top" src={imageUrl + movie.poster_path}/>
-					<Card.Body>
-						<Card.Title>{movie.title}</Card.Title>
-						<Card.Text className="d-flex justify-content-between">
-							⭐️ {movie.vote_average}
-							<Button 
-								as={Link} 
-								to={`/movies/${movie.id}`} 
-								variant="primary"
-								> Read more
-							</Button>
-						</Card.Text>
-					</Card.Body>
-				</Card>
-			</Col>
-		))}
+			{movies.results.map(movie => (
+				<Col lg={3} md={4} sm={6} key={movie.id}>
+						
+					<Card className="mb-4">
+						<Card.Img variant="top" src={imageUrl + movie.poster_path}/>
+						<Card.Body>
+							<Card.Title>{movie.title}</Card.Title>
+							<Card.Text className="d-flex justify-content-between">
+								⭐️ {movie.vote_average}
+								<Button 
+									as={Link} 
+									to={`/movies/${movie.id}`} 
+									variant="primary"
+									> Read more
+								</Button>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			))}
 		</Row>
-
   	)
 }
 
