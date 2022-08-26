a1796f99fa039171c66cf776155635b6d2b0eb50
50773sa <93046590+50773sa@users.noreply.github.com>
2022-08-26 11:24:02 +0200

    Added placeholder for img if profile_path is null.

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -4,17 +4,19 @@ import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import { Link } from 'react-router-dom'
+import Placeholder from 'react-bootstrap/Placeholder';
 
-const Person = ({ person, movies }) => {
+
+const Person = ({ person }) => {
 
 	const imageUrl = 'https://image.tmdb.org/t/p/w500'
-
+	console.log(person)
 
   	return (
 		<Row>
 			<Col lg={6} md={6} sm={6} key={person.id}>
 				<Card className="mb-4">
-					<Card.Img variant="left" src={imageUrl + person.profile_path} />
+					<Card.Img variant="left" src={person.profile_path !== null ? imageUrl + person.profile_path : 'https://via.placeholder.com/500' } />
 					<Card.Body>
 						<Card.Title>{person.name}</Card.Title>
 						<Card.Text>{person.biography}</Card.Text>
